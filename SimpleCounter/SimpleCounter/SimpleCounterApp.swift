4b8370a74e04eeb75c5e47b46c1bcc1735a198b2
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-04-13 20:05:43 +0200

    Initial Setup

---
+++ SimpleCounter/SimpleCounter/SimpleCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterApp.swift
+//  SimpleCounter
+//
+//  Created by Nikolas Huber on 13.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
