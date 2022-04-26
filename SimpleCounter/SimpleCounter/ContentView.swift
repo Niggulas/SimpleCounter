42c9426eb995581bbe9b302f5e8f6a23e923e77c
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-04-26 08:14:28 +0200

    Initial construction of UI

--- SimpleCounter/SimpleCounter/ContentView.swift
+++ SimpleCounter/SimpleCounter/ContentView.swift
@@ -14,26 +14,64 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Button {
-                    counterLeft += 1
-                } label: {
-                    Text(String(counterLeft))
-                        .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: .infinity, maxHeight: .infinity)
-                        .font(.custom("big", size: 100))
-                        .foregroundColor(Color.primary)
+                VStack {
+                    Button {
+                        counterLeft += 1
+                    } label: {
+                        Text(String(counterLeft))
+                            .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: .infinity, maxHeight: .infinity)
+                            .font(.custom("big", size: 150))
+                            .foregroundColor(Color.primary)
+                    }
+                    .background(Color.red)
+                    
+                    Divider()
+                    
+                    Button {
+                        if counterLeft > 0 {
+                            counterLeft -= 1
+                        }
+                    } label: {
+                        Text("-1")
+                            .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: 60, maxHeight: 60)
+                            .font(.custom("negative", size: 50))
+                            .background(Color.red)
+                            .foregroundColor(Color.primary)
+                    }
+
                 }
                 .background(Color.red)
                 
-                Button  {
-                    counterRight += 1
-                } label: {
-                    Text(String(counterRight))
-                        .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: .infinity, maxHeight: .infinity)
-                        .font(.custom("big", size: 100))
-                        .foregroundColor(Color.primary)
+                VStack {
+                    Button  {
+                        counterRight += 1
+                    } label: {
+                        Text(String(counterRight))
+                            .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: .infinity, maxHeight: .infinity)
+                            .font(.custom("big", size: 150))
+                            .foregroundColor(Color.primary)
+                    }
+                    .background(Color.green)
+                    
+                    Divider()
+                    
+                    Button {
+                        if counterRight > 0 {
+                            counterRight -= 1
+                        }
+                    } label: {
+                        Text("-1")
+                            .frame(minWidth: 15, idealWidth: .infinity, maxWidth: .infinity, minHeight: 15, idealHeight: 60, maxHeight: 60)
+                            .font(.custom("negative", size: 50))
+                            .background(Color.green)
+                            .foregroundColor(Color.primary)
+                    }
+
                 }
                 .background(Color.green)
+                
             }
+            .background(Color.red)
             .ignoresSafeArea()
             
             Button {
@@ -43,12 +81,14 @@ struct ContentView: View {
                 Text("RESET")
                     .font(.custom("reset", size: 25))
                     .foregroundColor(Color.red)
-                    .frame(minWidth: 12, idealWidth: .infinity, maxWidth: .infinity, minHeight: 20, idealHeight: 30, maxHeight: 30)
+                    .frame(minWidth: 12, idealWidth: .infinity, maxWidth: .infinity, minHeight: 20, idealHeight: 60, maxHeight: 60)
                     .background(Color(.systemGray6))
             }
 
             
         }
+        .ignoresSafeArea()
+        .background(Color(.systemGray6))
     }
 }
 
